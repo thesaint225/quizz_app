d43288fe07e3783cbd5d5cfa2dadbefbced9aa4a
henrymiessan <miessanhenri@gmail.com>
2025-04-21 01:59:00 +0000

    docs: Add route documentation for quiz creation and retrieval

--- backendQuizz/src/routes/quizRoutes.ts
+++ backendQuizz/src/routes/quizRoutes.ts
@@ -2,7 +2,12 @@ import { Router } from "express";
 import { createQuiz, getAllQuiz } from "../controllers/quizController";
 
 const QuizRouter = Router();
+// @desc   Create a new quiz
+// @route  POST/api/v1/quizzes
+QuizRouter.post("/", createQuiz);
 
-QuizRouter.post("/", createQuiz).get("/", getAllQuiz);
+// @desc   getting all quizzes
+// @route  GET/api/v1/quizzes
+QuizRouter.get("/", getAllQuiz);
 
 export default QuizRouter;
