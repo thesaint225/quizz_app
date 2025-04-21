ca818ef0021082fa6179895213de7596111c54fc
henrymiessan <miessanhenri@gmail.com>
2025-04-21 01:50:11 +0000

    feat(router): add POST and GET routes for questions

---
+++ backendQuizz/src/routes/questionRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import {
+  createQuestion,
+  getAllQuestions,
+} from "../controllers/questionController";
+
+const questionRouter = Router();
+// @desc   Create a new question
+// @route  POST/api/v1/questions
+questionRouter.post("/", createQuestion);
+
+// @desc   get all questions
+// @route  GET/api/v1/questions
+questionRouter.get("/", getAllQuestions);
