46f6947cfaadd1a97ee44a4c42a21d867f6ad8af
henrymiessan <miessanhenri@gmail.com>
2025-07-25 15:33:04 +0000

    feat: set up project structure and routing for French quiz app

---
+++ quiz-frontend/src/pages/QuizPage.tsx
@@ -0,0 +1,7 @@
+export default function QuizPage() {
+  return (
+    <div>
+      <h1>this is the the quiz Page </h1>
+    </div>
+  );
+}
---
+++ quiz-frontend/src/pages/ResultsPage.tsx
@@ -0,0 +1,7 @@
+export default function ResultsPage() {
+  return (
+    <div>
+      <h1>this is the result page </h1>
+    </div>
+  );
+}
